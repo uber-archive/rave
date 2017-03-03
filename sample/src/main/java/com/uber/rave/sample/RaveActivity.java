ad5d594fa59a03e2e44122e88a70384587617318
Eric Leung <eleung@uber.com>
2017-03-03 02:39:39 -0800

    add serialization/deserialization as an alternate way of demonstrating RAVEs use case

--- sample/src/main/java/com/uber/rave/sample/RaveActivity.java
+++ sample/src/main/java/com/uber/rave/sample/RaveActivity.java
@@ -1,13 +1,23 @@
 package com.uber.rave.sample;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Toast;
 
+import com.uber.rave.Rave;
 import com.uber.rave.RaveException;
 import com.uber.rave.sample.github.GitHubModule;
 import com.uber.rave.sample.github.GitHubService;
+import com.uber.rave.sample.github.model.Owner;
 import com.uber.rave.sample.github.model.Repo;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 import javax.inject.Inject;
 
@@ -37,19 +47,86 @@ public class RaveActivity extends Activity {
                 .enqueue(new Callback<Repo>() {
                     @Override
                     public void onResponse(Call<Repo> call, Response<Repo> response) {
-                        makeToast("Call passed Rave validation!");
+                        makeToast("Call passed RAVE validation!");
                     }
 
                     @Override
                     public void onFailure(Call<Repo> call, Throwable t) {
                         Throwable cause = t.getCause();
                         if (cause instanceof RaveException) {
-                            makeToast("Call did not pass Rave validation: " + cause.getMessage());
+                            makeToast("Call did not pass RAVE validation: " + cause.getMessage());
                         } else if (cause != null) {
                             makeToast("Call failed for unknown reason: " + cause.getMessage());
                         }
                     }
                 });
+    }
+
+    @OnClick(R.id.storage_request)
+    void fetchFromStorage() {
+        Owner original = new Owner();
+
+        //Commenting out the line below will cause this to fail RAVE validation. This emulates if you had invalid
+        //date serialized and stored in storage, and retrieved at a later point. Though the object exists, it is no
+        //longer valid with our new Nullness Annotations.
+        original.setLogin("abc");
+        original.setId(1);
+
+        Owner fromStorage = deserialize(serialize(original));
+        if (fromStorage == null) {
+            makeToast("Could not serialize/deserialize");
+            return;
+        }
+
+        if (!fromStorage.equals(original)) {
+            makeToast("Serialization/deserialization had problems, original and new object not equal.");
+            return;
+        }
+
+        try {
+            Rave.getInstance().validate(fromStorage);
+            makeToast("Storage object passed RAVE validation");
+        } catch (RaveException e) {
+            makeToast("Storage object failed RAVE validation");
+        }
+    }
+
+    private byte[] serialize(Owner owner) {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(bos);
+            out.writeObject(owner);
+            out.flush();
+            return bos.toByteArray();
+        } catch (IOException ignored) {
+            return new byte[0];
+        } finally {
+            try {
+                bos.close();
+            } catch (IOException ex) {
+                // ignore close exception
+            }
+        }
+    }
+
+    private Owner deserialize(byte[] bytes) {
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        ObjectInputStream in = null;
+
+        try {
+            in = new ObjectInputStream(bis);
+            return (Owner) in.readObject();
+        } catch (ClassNotFoundException | IOException e) {
+            return null;
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                // ignore close exception
+            }
+        }
     }
 
     private void makeToast(String text) {
--- sample/src/main/java/com/uber/rave/sample/github/model/Owner.java
+++ sample/src/main/java/com/uber/rave/sample/github/model/Owner.java
@@ -5,8 +5,10 @@ import android.support.annotation.NonNull;
 import com.uber.rave.annotation.Validated;
 import com.uber.rave.sample.github.RaveValidatorFactory;
 
+import java.io.Serializable;
+
 @Validated(factory = RaveValidatorFactory.class)
-public class Owner {
+public class Owner implements Serializable {
 
     @NonNull private String login;
     private int id;
@@ -28,4 +30,22 @@ public class Owner {
         this.id = id;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Owner owner = (Owner) o;
+
+        if (id != owner.id) return false;
+        return login != null ? login.equals(owner.login) : owner.login == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = login != null ? login.hashCode() : 0;
+        result = 31 * result + id;
+        return result;
+    }
 }
