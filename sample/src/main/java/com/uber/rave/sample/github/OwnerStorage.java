956c7918c84ce267cbd3185e1eb699893f23e745
Eric Leung <eleung@uber.com>
2017-03-06 02:03:36 -0800

    clean up storage class api

--- sample/src/main/java/com/uber/rave/sample/github/OwnerStorage.java
+++ sample/src/main/java/com/uber/rave/sample/github/OwnerStorage.java
@@ -14,20 +14,44 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 /**
- * This uses the default Java serializer.
+ * This uses the default Java serializer. This simulate storing the object to disk, but only to memory for
+ * demo purposes.
  */
 public class OwnerStorage {
-    byte[] serializedOwner = new byte[0];
+    private byte[] serializedOwner = new byte[0];
 
+    /**
+     * "Store" the owner object.
+     *
+     * @param owner object to store.
+     */
     public void storeOwner(Owner owner) {
+        serializedOwner = serialize(owner);
+    }
+
+    /**
+     * Get the "stored" owner object.
+     *
+     * @return "stored" owner object.
+     */
+    public Owner getOwner() {
+        return deserialize(serializedOwner);
+    }
+
+    /**
+     * Serialize a given Owner object into a byte array.
+     * @param owner {@link Owner} object to serialize.
+     * @return serialized owner as a byte array
+     */
+    private byte[] serialize(Owner owner) {
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         try {
             ObjectOutputStream out = new ObjectOutputStream(bos);
             out.writeObject(owner);
             out.flush();
-            serializedOwner = bos.toByteArray();
+            return bos.toByteArray();
         } catch (IOException ignored) {
-
+            return new byte[0];
         } finally {
             try {
                 bos.close();
@@ -37,12 +61,17 @@ public class OwnerStorage {
         }
     }
 
-    public Owner getOwner() {
+    /**
+     * Deserialize a byte array to an Owner object.
+     * @param bytes byte array to deserialize
+     * @return {@link Owner} object deserialized from byte array
+     */
+    private Owner deserialize(byte[] bytes) {
         if (serializedOwner.length == 0) {
             return null;
         }
 
-        ByteArrayInputStream bis = new ByteArrayInputStream(serializedOwner);
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
         ObjectInputStream in = null;
 
         try {
