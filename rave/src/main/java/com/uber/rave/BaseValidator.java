6dde71bd1e7649bfc884eff8c4d70b7d8dcf6ca8
Behrooz Khorashadi <behrooz@uber.com>
2017-05-03 13:56:49 -0700

    Fixed minor bug.

--- rave/src/main/java/com/uber/rave/BaseValidator.java
+++ rave/src/main/java/com/uber/rave/BaseValidator.java
@@ -192,7 +192,6 @@ public abstract class BaseValidator {
         if (collection == null) {
             return raveErrors;
         }
-        raveErrors = checkIterable(collection, null);
         int collectionSize = collection.size();
         raveErrors = testMultipleParameter(multiple, collectionSize, validationContext,
                 collection.getClass().getCanonicalName(), raveErrors);
@@ -224,19 +223,6 @@ public abstract class BaseValidator {
         List<RaveError> raveErrors = checkNullable(array, isNullable, validationContext);
         if (array == null) {
             return raveErrors;
-        }
-        Rave rave = Rave.getInstance();
-        for (T type : array) {
-            if (type == null) {
-                continue;
-            }
-            try {
-                rave.validate(type);
-            } catch (UnsupportedObjectException e) {
-                break;
-            } catch (RaveException e) {
-                raveErrors = appendErrors(e, raveErrors);
-            }
         }
         raveErrors = testMultipleParameter(multiple, array.length, validationContext, "", raveErrors);
         if (array.length <= max && array.length >= min) {
