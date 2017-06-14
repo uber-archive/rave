37067bb89b9aa22354fbc02b0559994a8f314576
Behrooz Khorashadi <behrooz@uber.com>
2017-06-14 10:46:41 -0700

    Reset the Owner Pojo to remove the unnecessary changes.

--- sample/src/main/java/com/uber/rave/sample/network/model/Owner.java
+++ sample/src/main/java/com/uber/rave/sample/network/model/Owner.java
@@ -2,27 +2,16 @@ package com.uber.rave.sample.network.model;
 
 import android.support.annotation.NonNull;
 
-import com.uber.rave.InvalidModelException;
-import com.uber.rave.annotation.MustBeTrue;
 import com.uber.rave.annotation.Validated;
 import com.uber.rave.sample.network.RaveValidatorFactory;
-import com.uber.rave.sample.network.RaveValidatorFactory_Generated_Validator;
 
 import java.io.Serializable;
-import java.util.List;
 
 @Validated(factory = RaveValidatorFactory.class)
 public class Owner implements Serializable {
-    private static final String finalstring = "";
-    @NonNull private String login;
-    @MustBeTrue private boolean field2;
-    private final List<Owner> list = null;
-    private int id;
 
-    public Owner() {
-        login = "";
-        field2 = false;
-    }
+    @NonNull private String login;
+    private int id;
 
     @NonNull
     public String getLogin() {
@@ -57,14 +46,5 @@ public class Owner implements Serializable {
         int result = login != null ? login.hashCode() : 0;
         result = 31 * result + id;
         return result;
-    }
-
-    // This is an example of how you can check inner fields that are protected or private. We will soon eliminat the
-    // need to even do this in later RAVE releases.
-    @MustBeTrue
-    public boolean validateFields() throws InvalidModelException {
-        RaveValidatorFactory_Generated_Validator.validateInternalFor_com_uber_rave_sample_network_model_Owner(login,
-                field2, list);
-        return true;
     }
 }
